a4ba318cff993617a02d366263665f03fe67780f
SittipongJuengsakul <stp55160555@gmail.com>
2015-01-09 14:25:43 +0700

    File C++ Example for exam ข้อ 1000 Signed-off-by:SittipongJuengsakul <stp55160555@gmail.com>

---
+++ CppApplication1000/main1000.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main1000.cpp
+ * Author: dImOndTH
+ *
+ * Created on 9 มกราคม 2558, 14:22 น.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World";
+    return 0;
+}
+
