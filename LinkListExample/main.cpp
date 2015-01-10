be1d0a07282b182580218774e5c944334c8dd036
SittipongJuengsakul <stp55160555@gmail.com>
2015-01-10 13:02:17 +0700

    ทดสอบ Double LinkList ทำไหม่ Signed-off-by:SittipongJuengsakul <stp55160555@gmail.com>

--- LinkListExample/main.cpp
+++ LinkListExample/main.cpp
@@ -13,22 +13,36 @@
 int main(int argc, char** argv) {
     node *temp; //เก็บตำแหน่งของโหนดที่กำลังอยู่
     node *head; //เก็บตำแหน่งแรกของโหนด
+    node *tail; //เก็บโหนดสุดท้ายของโหนด
+    int num = 0;
     
-    head = new node;
-    head->next = 0;
-    head->prev = 0;
-    head->x = "REAR";
-    temp = head;
+    temp = new node;
+    temp->x = "REAR";
+    temp->prev = NULL;
+    head = temp;
+    tail = temp;
+    num = num+1;
     
-    temp->next = new node;
-    temp->prev = head;
-    temp = temp->next;
-    temp->next = 0;
+    temp = new node;
     temp->x = "CEAR";
+    temp->prev = tail;
+    tail->next = temp;
+    tail = temp;
+    num =  num+1;
     
-    cout <<"Value head is "<< head->x << endl;;
-    cout <<"Value temp is "<< temp->x;
-    //cout <<"Value temp is "<< temp->prev->x;
+    temp = new node;
+    temp->x = "XEAR";
+    temp->prev = tail;
+    tail->next = temp;
+    tail = temp;
+    num =  num+1;
+    tail->next = NULL;
+    
+    int i;for(i=1;i<=num;i++){
+        cout<<"Node "<<i<<" = " << tail->x<<endl;
+        tail = tail->prev;
+    }
+    
     return 0;
 }
 
