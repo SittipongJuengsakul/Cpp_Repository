8392a47f8ba49522102abc42b007a0905d2e875c
SittipongJuengsakul <stp55160555@gmail.com>
2015-01-10 20:06:38 +0700

    ข้อ 1002 : ROMAN สร้างไฟร์ C++ และ Header เขียนโจทย์และออกแบบอังกอริทึ่ม Signed-off-by:SittipongJuengsakul <stp55160555@gmail.com>

---
+++ CppApplication1002/Header1002.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   Header1002.h
+ * Author: dImOndTH
+ *
+ * Created on 10 มกราคม 2558, 19:14 น.
+ */
+
+#ifndef HEADER1002_H
+#define	HEADER1002_H
+#include <iostream>
+#include <string>
+#include <cstdlib>
+struct node{
+    
+};
+
+
+#endif	/* HEADER1002_H */
+
