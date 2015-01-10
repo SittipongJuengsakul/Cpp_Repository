53b2c37b402aca40f6b1268d676e4340180dca47
SittipongJuengsakul <stp55160555@gmail.com>
2015-01-10 13:54:40 +0700

    เปลี่ยนข้อ 1000 เป็น LinkList ไห้รับข้อมูลได้เรื่อยๆและเป็นระเบียบ Signed-off-by:SittipongJuengsakul <stp55160555@gmail.com>

---
+++ CppApplication1000/hmain.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   hmain.h
+ * Author: dImOndTH
+ *
+ * Created on 10 มกราคม 2558, 10:47 น.
+ */
+
+#ifndef HMAIN_H
+#define	HMAIN_H
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct node {
+  string x;
+  node *next;
+  node *prev;
+};
+
+#endif	/* HMAIN_H */
+
--- CppApplication1000/main1000.cpp
+++ CppApplication1000/main1000.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include "hmain.h"
 
 using namespace std;
 
@@ -19,71 +20,9 @@ using namespace std;
  *  3.ถ้ามีตัวอักษรตำแหน่งเดียวกันเป็นตัวอักษรเดียวกัน อย่างน้อย 2 ตำแหน่ง  : ทำซำเรื่อยๆ จากข้อ 2
  *  4.ถ้าตัวอักษรตำแหน่งเดียวกันเป็นตัวอักษรเดียวกัน น้อยกว่า 2 ตำแหน่ง : โซ่คำหยุด และ แสดงตัวอักษรนั้นบนหน้าจอ
  */
-int EnterLineOne();
-int EnterLineTwo();
-string EnterEverLine();
-int WordChain(string ,string);
 int main(int argc, char** argv) {
-    string WordA;
-    string WordB;
-    int WordChainCount;
-    int L = EnterLineOne();
-    if(L<3 || L>1000){
-        cout << "Error L<3 and L>1000";
-        return 0;     
-    }
-    int N = EnterLineTwo();
-    if(N<1 || N>30000){
-        cout << "Error N<1 and N>30000";
-        return 0;
-    }
-    WordA = EnterEverLine();
-    
-    CycleN:WordB = EnterEverLine();
-    WordChainCount = WordChain(WordA,WordB);
     
     
-    if(WordChainCount>=2){
-        cout<< "CC = :" << WordChainCount;
-        WordChainCount = 0;
-        WordA = WordB;
-        goto CycleN;
-    }
-    else{
-        cout<<"End Of Chain Word : "+WordB;
-        cout<< "CC = :" << WordChainCount;
-    }
     
     return 0;
-}
-
-int EnterLineOne(){
-    int L;
-    cout << "Enter Num of L (3<=L<=1000 : ";
-    cin >> L;
-    return L;
-}
-int EnterLineTwo(){
-    int N;
-    cout << "Enter Num of N (1<=N<=30000 : ";
-    cin >> N;
-    return N;
-}
-string EnterEverLine(){
-    string Word;
-    cout << "Enter Word 4 character : ";
-    cin >> Word;
-    return Word;
-}
-int WordChain(string WordA,string WordB){
-    int IndexOfWord;
-    int KeepChainLeg = 0;
-    int CycleOfString = 0;
-    
-    for(CycleOfString=0;CycleOfString<=3;CycleOfString++){
-        if(WordA[CycleOfString]==WordB[CycleOfString]){
-            KeepChainLeg = KeepChainLeg+1;
-        }
-    }
-    return KeepChainLeg;
 }
